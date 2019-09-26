1159b9865fbc351b4ab05500e11635d81c5032cd
JBrandonRS <55464790+JBrandonRS@users.noreply.github.com>
2019-09-26 17:49:05 -0500

    Add files via upload
    Envío de parámetros por GET en URL

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+var array = [1,2,3];
+var personas = {Nombre: "", Edad: 0 };
 
 //Handle requests to http://127.0.0.1:3000
-app.get("/", (req, rest) => {
-    rest.send("Hola muundo");
+app.get("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send();
+});
+
+app.get("/:Nombre"+"/:Edad", (req, res) => {
+    personas.Nombre = req.params.Nombre;
+    personas.Edad = req.params.Edad;
+    console.log(personas);
 });
 
 //Run Server
